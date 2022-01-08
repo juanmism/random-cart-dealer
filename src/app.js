abdaf1727a633f1285b81864eb337c9242ec8729
juanmism <juanmism@gmail.com>
2022-01-08 18:20:18 +0000

    firstCommit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,69 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let icon = ["♦", "♥", "♠", "♣"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function randomCards() {
+  let iconIndex = Math.floor(Math.random() * 4);
+  let numberCard = Math.floor(Math.random() * 12 + 1);
+  let contentCard = icon[iconIndex];
+  if (numberCard < 10 && (iconIndex === 0 || iconIndex === 1)) {
+    document.getElementById(
+      "xcard"
+    ).innerHTML = `<div class="headCard red">${contentCard}</div><div class="bodyCard">${numberCard}</div><div class="footCard red">${contentCard}</div>`;
+  }
+  if (numberCard < 10 && (iconIndex === 2 || iconIndex === 3)) {
+    document.getElementById(
+      "xcard"
+    ).innerHTML = `<div class="headCard black">${contentCard}</div><div class="bodyCard">${numberCard}</div><div class="footCard black">${contentCard}</div>`;
+  }
+
+  if (numberCard === 10 && (iconIndex === 0 || iconIndex === 1)) {
+    document.getElementById(
+      "xcard"
+    ).innerHTML = `<div class="headCard red">${contentCard}J</div><div class="bodyCard">${numberCard}</div><div class="footCard red">J${contentCard}</div>`;
+  }
+  if (numberCard === 10 && (iconIndex === 2 || iconIndex === 3)) {
+    document.getElementById(
+      "xcard"
+    ).innerHTML = `<div class="headCard black">${contentCard}J</div><div class="bodyCard">${numberCard}</div><div class="footCard black">J${contentCard}</div>`;
+  }
+  if (numberCard === 11 && (iconIndex === 0 || iconIndex === 1)) {
+    document.getElementById(
+      "xcard"
+    ).innerHTML = `<div class="headCard red">${contentCard}Q</div><div class="bodyCard">${numberCard}</div><div class="footCard red">Q${contentCard}</div>`;
+  }
+  if (numberCard === 11 && (iconIndex === 2 || iconIndex === 3)) {
+    document.getElementById(
+      "xcard"
+    ).innerHTML = `<div class="headCard black">${contentCard}Q</div><div class="bodyCard">${numberCard}</div><div class="footCard black">Q${contentCard}</div>`;
+  }
+  if (numberCard === 12 && (iconIndex === 0 || iconIndex === 1)) {
+    document.getElementById(
+      "xcard"
+    ).innerHTML = `<div class="headCard red">${contentCard}K</div><div class="bodyCard">${numberCard}</div><div class="footCard red">K${contentCard}</div>`;
+  }
+  if (numberCard === 12 && (iconIndex === 2 || iconIndex === 3)) {
+    document.getElementById(
+      "xcard"
+    ).innerHTML = `<div class="headCard black">${contentCard}K</div><div class="bodyCard">${numberCard}</div><div class="footCard black">K${contentCard}</div>`;
+  }
+}
+window.onload = randomCards();
+
+let button = document.querySelector("#button");
+button.addEventListener("click", randomCards);
+
+let button2 = document.querySelector("#button2");
+button2.addEventListener("click", ActivarTiempo);
+
+let xx;
+function ActivarTiempo() {
+  xx = setInterval(randomCards, 10000);
+  let button3 = document.querySelector("#button3");
+  button3.addEventListener("click", PararTiempo);
+}
+
+function PararTiempo() {
+  clearInterval(xx);
+}
